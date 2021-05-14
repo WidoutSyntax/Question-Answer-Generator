3726220606e120fb7279b882974b78090848e4af
WidoutSyntax <30340122+WidoutSyntax@users.noreply.github.com>
2021-05-14 13:37:00 +0530

    Delete unwanted files

--- inputReader.py
+++
@@ -1,9 +0,0 @@
-with open('./data/inputData.txt') as f:
-    lines = f.readlines()
-ans = ""
-#print(lines)
-for i in range(0, len(lines)-1):
-    if(len(lines[i]) >= 2 ):
-        ans = ans + lines[i][0: len(lines[i])-2]
-ans = ans + ". " + lines[len(lines)-1]
-#print (ans)
